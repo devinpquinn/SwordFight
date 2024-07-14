3a6659390313669edbc1bb7cc268f5d39d42f543
Devin <devinpquinn4@gmail.com>
2024-07-14 16:34:32 -0400

    tmpro

--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -15,6 +15,9 @@ public class FightManager : MonoBehaviour
     public Image playerStaminaBar;
     public Image enemyStaminaBar;
 
+    public TMPro.TextMeshProUGUI playerStaminaLabel;
+    public TMPro.TextMeshProUGUI enemyStaminaLabel;
+
     private int position;
     public Image[] trackMarkers = new Image[5];
 
@@ -66,6 +69,8 @@ public class FightManager : MonoBehaviour
             Debug.Log("DISENGAGE!");
             playerStamina = enemyStamina = maxStamina;
             position = 3;
+
+            UpdateLabels("<color=yellow>Disengage!</color>", "<color=yellow>Disengage!</color>");
         }
         else
         {
@@ -92,25 +97,9 @@ public class FightManager : MonoBehaviour
                 }
             }
 
-            if(playerSpent == enemySpent)
+            if(playerSpent != enemySpent)
             {
-                //clash
-                Debug.Log("CLASH!");
-
-                if(Random.Range(0, 2) == 1)
-                {
-                    //player wins clash
-                    playerSpent++;
-                }
-                else
-                {
-                    //enemy wins clash
-                    enemySpent++;
-                }
-            }
-            else
-            {
-                if(playerSpent > enemySpent)
+                if (playerSpent > enemySpent)
                 {
                     //player pushes back enemy
                     position++;
@@ -119,21 +108,51 @@ public class FightManager : MonoBehaviour
                 {
                     //enemy pushes back player
                     position--;
-                }
+                } 
+            }
+            else
+            {
+                //clash
+                Debug.Log("CLASH!");
 
-                //check for wound
-                if(position > 5)
+                if (Random.Range(0, 2) == 1)
                 {
-                    Debug.Log("ENEMY HIT!");
-                    playerStamina = enemyStamina = maxStamina;
-                    position = 3;
+                    //player wins clash
+                    position++;
+                    UpdateLabels("<color=green>CLASH " + playerSpent + " - Win!</color>", "<color=red>CLASH " + enemySpent + " - Lose!</color>");
                 }
-                else if(position < 1)
+                else
                 {
-                    Debug.Log("PLAYER HIT!");
-                    playerStamina = enemyStamina = maxStamina;
-                    position = 3;
+                    //enemy wins clash
+                    position--;
+                    UpdateLabels("<color=red>CLASH " + playerSpent + " - Lose!</color>", "<color=green>CLASH " + enemySpent + " - Win!</color>");
                 }
+            }
+
+            //check for wound
+            if (position > 5)
+            {
+                Debug.Log("ENEMY HIT!");
+                playerStamina = enemyStamina = maxStamina;
+                position = 3;
+
+                UpdateLabels("<color=green>" + playerSpent.ToString() + "</color>", "<color=red>" + (enemySpent < 0 ? "Defend" : enemySpent) + " - HIT!!</color>");
+            }
+            else if (position < 1)
+            {
+                Debug.Log("PLAYER HIT!");
+                playerStamina = enemyStamina = maxStamina;
+                position = 3;
+
+                UpdateLabels("<color=red>" + (playerSpent < 0 ? "Defend" : playerSpent) + " - HIT!!</color>", "<color=green>" + enemySpent.ToString() + "</color>");
+            }
+            else if(playerSpent > enemySpent)
+            {
+                UpdateLabels("<color=green>" + playerSpent + " - Win!</color>", "<color=red>" + (enemySpent < 0 ? "Defend" : enemySpent) + " - Lose!</color>");
+            }
+            else if (enemySpent > playerSpent)
+            {
+                UpdateLabels("<color=red>" + (playerSpent < 0 ? "Defend" : playerSpent) + " - Lose!</color>", "<color=green>" + enemySpent + " - Win!</color>");
             }
         }
 
@@ -148,6 +167,18 @@ public class FightManager : MonoBehaviour
     {
         playerStaminaBar.fillAmount = (float) playerAmt / maxStamina;
         enemyStaminaBar.fillAmount = (float) enemyAmt / maxStamina;
+    }
+
+    public void UpdateLabels(string playerLabel, string enemyLabel)
+    {
+        playerStaminaLabel.text = playerLabel;
+        enemyStaminaLabel.text = enemyLabel;
+
+        playerStaminaLabel.gameObject.SetActive(false);
+        playerStaminaLabel.gameObject.SetActive(true);
+
+        enemyStaminaLabel.gameObject.SetActive(false);
+        enemyStaminaLabel.gameObject.SetActive(true);
     }
 
     public void UpdateTrack(int pos)
